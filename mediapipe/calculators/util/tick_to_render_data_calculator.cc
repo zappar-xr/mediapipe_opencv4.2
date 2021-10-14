fd805e2a664363f571c9b120aa952665a1bbf7f3
tjiang <tjiang.work@gmail.com>
2021-10-14 21:59:38 +0100

    tick to render data calculator is compiled

---
+++ mediapipe/calculators/util/tick_to_render_data_calculator.cc
@@ -0,0 +1,106 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <math.h>
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/status.h"
+#include "mediapipe/framework/port/statusor.h"
+#include "mediapipe/util/color.pb.h"
+#include "mediapipe/util/render_data.pb.h"
+#include "mediapipe/framework/port/opencv_highgui_inc.h"
+
+namespace mediapipe
+{
+
+  constexpr char kRenderDataTag[] = "RENDER_DATA";
+  constexpr char kTickTag[] = "TICK";
+
+  constexpr float kFontHeightScale = 1.25f;
+
+  // A calculator takes in pairs of labels and scores or classifications, outputs
+  // generates render data. Either both "LABELS" and "SCORES" or "CLASSIFICATIONS"
+  // must be present.
+  //
+  // Usage example:
+  // node {
+  //   calculator: "TickToRenderDataCalculator"
+  //   input_stream: "TICK:tick"
+  //   output_stream: "RENDER_DATA:fps_render_data"
+  //   }
+  // }
+  class TickToRenderDataCalculator : public CalculatorBase
+  {
+  public:
+    static absl::Status GetContract(CalculatorContract *cc);
+    absl::Status Open(CalculatorContext *cc) override;
+    absl::Status Process(CalculatorContext *cc) override;
+
+  private:
+    int num_colors_ = 0;
+    int video_width_ = 0;
+    int video_height_ = 0;
+    int label_height_px_ = 0;
+    int label_left_px_ = 0;
+  };
+  REGISTER_CALCULATOR(TickToRenderDataCalculator);
+
+  absl::Status TickToRenderDataCalculator::GetContract(CalculatorContract *cc)
+  {
+
+    cc->Inputs().Tag(kTickTag).Set<int64>();
+    cc->Outputs().Tag(kRenderDataTag).Set<RenderData>();
+
+    return absl::OkStatus();
+  }
+
+  absl::Status TickToRenderDataCalculator::Open(CalculatorContext *cc)
+  {
+    cc->SetOffset(TimestampDiff(0));
+    return absl::OkStatus();
+  }
+
+  absl::Status TickToRenderDataCalculator::Process(CalculatorContext *cc)
+  {
+    
+    const auto& tick_start = cc->Inputs().Tag(kTickTag).Get<int64>();
+    auto output_fps = cv::getTickFrequency() / (cv::getTickCount() - tick_start);
+
+    RenderData render_data;
+    auto *label_annotation = render_data.add_render_annotations();
+    label_annotation->set_thickness(10.0);
+    label_annotation->mutable_color()->set_r(0);
+    label_annotation->mutable_color()->set_g(255);
+    label_annotation->mutable_color()->set_b(0);
+    auto *text = label_annotation->mutable_text();
+    std::string display_text = std::to_string(output_fps);
+    text->set_display_text(display_text);
+    text->set_font_height(50);
+    text->set_left(50);
+    text->set_baseline(50);
+    text->set_font_face(1);
+
+    cc->Outputs()
+        .Tag(kRenderDataTag)
+        .AddPacket(MakePacket<RenderData>(render_data).At(cc->InputTimestamp()));
+
+    return absl::OkStatus();
+  }
+} // namespace mediapipe
