f9b6db87647974857d5434b217475b2ea3b0c288
tjiang <tjiang.work@gmail.com>
2021-10-14 06:45:28 +0100

    tmp save

--- mediapipe/examples/desktop/hello_world/hello_world.cc
+++ mediapipe/examples/desktop/hello_world/hello_world.cc
@@ -23,95 +23,113 @@
 #include "mediapipe/framework/stream_handler/fixed_size_input_stream_handler.pb.h"
 #include "mediapipe/framework/port/threadpool.h"
 
-
-namespace mediapipe {
-    class FixedPassThroughCalculator : public CalculatorBase {
- public:
-  static absl::Status GetContract(CalculatorContract* cc) {
-    if (!cc->Inputs().TagMap()->SameAs(*cc->Outputs().TagMap())) {
-      return absl::InvalidArgumentError(
-          "Input and output streams to PassThroughCalculator must use "
-          "matching tags and indexes.");
-    }
-    for (CollectionItemId id = cc->Inputs().BeginId();
-         id < cc->Inputs().EndId(); ++id) {
-      cc->Inputs().Get(id).SetAny();
-      cc->Outputs().Get(id).SetSameAs(&cc->Inputs().Get(id));
-    }
-    for (CollectionItemId id = cc->InputSidePackets().BeginId();
-         id < cc->InputSidePackets().EndId(); ++id) {
-      cc->InputSidePackets().Get(id).SetAny();
-    }
-    if (cc->OutputSidePackets().NumEntries() != 0) {
-      if (!cc->InputSidePackets().TagMap()->SameAs(
-              *cc->OutputSidePackets().TagMap())) {
+namespace mediapipe
+{
+  class FixedPassThroughCalculator : public CalculatorBase
+  {
+  public:
+    static absl::Status GetContract(CalculatorContract *cc)
+    {
+      if (!cc->Inputs().TagMap()->SameAs(*cc->Outputs().TagMap()))
+      {
         return absl::InvalidArgumentError(
-            "Input and output side packets to PassThroughCalculator must use "
+            "Input and output streams to PassThroughCalculator must use "
             "matching tags and indexes.");
       }
+      for (CollectionItemId id = cc->Inputs().BeginId();
+           id < cc->Inputs().EndId(); ++id)
+      {
+        cc->Inputs().Get(id).SetAny();
+        cc->Outputs().Get(id).SetSameAs(&cc->Inputs().Get(id));
+      }
       for (CollectionItemId id = cc->InputSidePackets().BeginId();
-           id < cc->InputSidePackets().EndId(); ++id) {
-        cc->OutputSidePackets().Get(id).SetSameAs(
-            &cc->InputSidePackets().Get(id));
+           id < cc->InputSidePackets().EndId(); ++id)
+      {
+        cc->InputSidePackets().Get(id).SetAny();
       }
+      if (cc->OutputSidePackets().NumEntries() != 0)
+      {
+        if (!cc->InputSidePackets().TagMap()->SameAs(
+                *cc->OutputSidePackets().TagMap()))
+        {
+          return absl::InvalidArgumentError(
+              "Input and output side packets to PassThroughCalculator must use "
+              "matching tags and indexes.");
+        }
+        for (CollectionItemId id = cc->InputSidePackets().BeginId();
+             id < cc->InputSidePackets().EndId(); ++id)
+        {
+          cc->OutputSidePackets().Get(id).SetSameAs(
+              &cc->InputSidePackets().Get(id));
+        }
+      }
+
+      // Assign this calculator's InputStreamHandler and options.
+      // cc->SetInputStreamHandler("FixedSizeInputStreamHandler");
+      // MediaPipeOptions options;
+      // options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
+      //     ->set_fixed_min_size(2);
+      // options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
+      //     ->set_trigger_queue_size(2);
+      // options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
+      //     ->set_target_queue_size(2);
+      // cc->SetInputStreamHandlerOptions(options);
+
+      return absl::OkStatus();
     }
 
-    // Assign this calculator's InputStreamHandler and options.
-    cc->SetInputStreamHandler("FixedSizeInputStreamHandler");
-    MediaPipeOptions options;
-    options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
-        ->set_fixed_min_size(2);
-    options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
-        ->set_trigger_queue_size(2);
-    options.MutableExtension(FixedSizeInputStreamHandlerOptions::ext)
-        ->set_target_queue_size(2);
-    cc->SetInputStreamHandlerOptions(options);
-
-    return absl::OkStatus();
-  }
-
-  absl::Status Open(CalculatorContext* cc) final {
-    for (CollectionItemId id = cc->Inputs().BeginId();
-         id < cc->Inputs().EndId(); ++id) {
-      if (!cc->Inputs().Get(id).Header().IsEmpty()) {
-        cc->Outputs().Get(id).SetHeader(cc->Inputs().Get(id).Header());
+    absl::Status Open(CalculatorContext *cc) final
+    {
+      for (CollectionItemId id = cc->Inputs().BeginId();
+           id < cc->Inputs().EndId(); ++id)
+      {
+        if (!cc->Inputs().Get(id).Header().IsEmpty())
+        {
+          cc->Outputs().Get(id).SetHeader(cc->Inputs().Get(id).Header());
+        }
       }
-    }
-    if (cc->OutputSidePackets().NumEntries() != 0) {
-      for (CollectionItemId id = cc->InputSidePackets().BeginId();
-           id < cc->InputSidePackets().EndId(); ++id) {
-        cc->OutputSidePackets().Get(id).Set(cc->InputSidePackets().Get(id));
+      if (cc->OutputSidePackets().NumEntries() != 0)
+      {
+        for (CollectionItemId id = cc->InputSidePackets().BeginId();
+             id < cc->InputSidePackets().EndId(); ++id)
+        {
+          cc->OutputSidePackets().Get(id).Set(cc->InputSidePackets().Get(id));
+        }
       }
+      cc->SetOffset(TimestampDiff(0));
+      return absl::OkStatus();
     }
-    cc->SetOffset(TimestampDiff(0));
-    return absl::OkStatus();
-  }
 
-  absl::Status Process(CalculatorContext* cc) final {
-    cc->GetCounter("PassThrough")->Increment();
-    if (cc->Inputs().NumEntries() == 0) {
-      return tool::StatusStop();
-    }
-    for (CollectionItemId id = cc->Inputs().BeginId();
-         id < cc->Inputs().EndId(); ++id) {
-      if (!cc->Inputs().Get(id).IsEmpty()) {
-        VLOG(3) << "Passing " << cc->Inputs().Get(id).Name() << " to "
-                << cc->Outputs().Get(id).Name() << " at "
-                << cc->InputTimestamp().DebugString();
-        cc->Outputs().Get(id).AddPacket(cc->Inputs().Get(id).Value());
+    absl::Status Process(CalculatorContext *cc) final
+    {
+      cc->GetCounter("PassThrough")->Increment();
+      if (cc->Inputs().NumEntries() == 0)
+      {
+        return tool::StatusStop();
       }
+      for (CollectionItemId id = cc->Inputs().BeginId();
+           id < cc->Inputs().EndId(); ++id)
+      {
+        if (!cc->Inputs().Get(id).IsEmpty())
+        {
+          VLOG(3) << "Passing " << cc->Inputs().Get(id).Name() << " to "
+                  << cc->Outputs().Get(id).Name() << " at "
+                  << cc->InputTimestamp().DebugString();
+          cc->Outputs().Get(id).AddPacket(cc->Inputs().Get(id).Value());
+        }
+      }
+      return absl::OkStatus();
     }
-    return absl::OkStatus();
-  }
-};
-REGISTER_CALCULATOR(FixedPassThroughCalculator);
-  
-    absl::Status PrintHelloWorld() {
-      // Configures a simple graph, which concatenates 2 PassThroughCalculators.
-      #define NUM_STREAMS 4
-  CalculatorGraphConfig graph_config =
-      mediapipe::ParseTextProtoOrDie<CalculatorGraphConfig>(
-          R"pb(
+  };
+      REGISTER_CALCULATOR(FixedPassThroughCalculator);
+  absl::Status PrintHelloWorld()
+  {
+
+// Configures a simple graph, which concatenates 2 PassThroughCalculators.
+#define NUM_STREAMS 4
+    CalculatorGraphConfig graph_config =
+        mediapipe::ParseTextProtoOrDie<CalculatorGraphConfig>(
+            R"pb(
             input_stream: "in_0"
             input_stream: "in_1"
             input_stream: "in_2"
@@ -126,49 +144,71 @@ REGISTER_CALCULATOR(FixedPassThroughCalculator);
               output_stream: "out_1"
               output_stream: "out_2"
               output_stream: "out_3"
+              input_stream_handler {
+                input_stream_handler: "FixedSizeInputStreamHandler"
+                options {
+                  [mediapipe.FixedSizeInputStreamHandlerOptions.ext] {
+                    trigger_queue_size: 4
+                    target_queue_size: 3
+                  }
+                }
+              }
               # FixedSizeInputStreamHandler set in GetContract()
             })pb");
-  std::vector<Packet> output_packets[NUM_STREAMS];
-  for (int i = 0; i < NUM_STREAMS; ++i) {
-    tool::AddVectorSink(absl::StrCat("out_", i), &graph_config,
-                        &output_packets[i]);
-  }
-  CalculatorGraph graph;
-  MP_RETURN_IF_ERROR(graph.Initialize(graph_config, {}));
-      MP_RETURN_IF_ERROR(graph.StartRun({}));
+    std::vector<Packet> output_packets[NUM_STREAMS];
+    for (int i = 0; i < NUM_STREAMS; ++i)
+    {
+      tool::AddVectorSink(absl::StrCat("out_", i), &graph_config,
+                          &output_packets[i]);
+    }
+    CalculatorGraph graph;
+    MP_RETURN_IF_ERROR(graph.Initialize(graph_config, {}));
+    MP_RETURN_IF_ERROR(graph.StartRun({}));
 
+    // {
+    //   mediapipe::ThreadPool pool(NUM_STREAMS);
+    //   pool.StartWorkers();
+
+      // Start writers.
+      for (int w = 0; w < NUM_STREAMS; ++w)
       {
-        mediapipe::ThreadPool pool(NUM_STREAMS);
-        pool.StartWorkers();
+        // pool.Schedule([&graph, w]()
+        //               {
+                        std::string stream_name = absl::StrCat("in_", w);
+                        for (int i = 0; i < 2; ++i)
+                        {
+                          Packet p = MakePacket<int>(i).At(Timestamp(i));
+                          MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(stream_name, p));
+                          absl::SleepFor(absl::Microseconds(100));
+                        }
+                      // });
+      }
+    // }
+    LOG(INFO) << "hello world";
+    std::cout << "hhhh" << std::endl;
+    MP_RETURN_IF_ERROR(graph.CloseAllInputStreams());
+    // MP_RETURN_IF_ERROR(graph.WaitUntilDone());
+    for (int i = 0; i < NUM_STREAMS; ++i)
+    {
+      // EXPECT_EQ(output_packets[i].size(), output_packets[0].size());
+      for (int j = 0; j < output_packets[i].size(); j++)
+      {
+        LOG(INFO) << output_packets[i][j].Get<int>() << 1;
+        // EXPECT_EQ(output_packets[i][j].Get<int>(),
+        //           output_packets[0][j].Get<int>());
+      }
+    }
 
-        // Start writers.
-        for (int w = 0; w < NUM_STREAMS; ++w) {
-          pool.Schedule([&, w]() {
-            std::string stream_name = absl::StrCat("in_", w);
-            for (int i = 0; i < 50; ++i) {
-              Packet p = MakePacket<int>(i).At(Timestamp(i));
-              MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(stream_name, p));
-              absl::SleepFor(absl::Microseconds(100));
-            }
-          });
-        }
-      }
-      MP_RETURN_IF_ERROR(graph.CloseAllInputStreams());
-      MP_RETURN_IF_ERROR(graph.WaitUntilDone());
-      for (int i = 0; i < NUM_STREAMS; ++i) {
-        // EXPECT_EQ(output_packets[i].size(), output_packets[0].size());
-        for (int j = 0; j < output_packets[i].size(); j++) {
-          LOG(INFO) << output_packets[i][j].Get<int>();
-          // EXPECT_EQ(output_packets[i][j].Get<int>(),
-          //           output_packets[0][j].Get<int>());
-        }
-      }
-        
-        return graph.WaitUntilDone();
-      }
-}  // namespace mediapipe
+    return graph.WaitUntilDone();
+  }
 
-int main(int argc, char** argv) {
+
+
+} // namespace mediapipe
+
+
+int main(int argc, char **argv)
+{
   google::InitGoogleLogging(argv[0]);
   CHECK(mediapipe::PrintHelloWorld().ok());
   return 0;
